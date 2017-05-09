86fed45f1ea3817f9680b1c03de2a34e9e8ea761
damanhanzo <singhdamanpreet1234@gmail.com>
2017-05-09 19:29:36 -0400

    updating py server

--- pyServer.py
+++ pyServer.py
@@ -1,46 +1,40 @@
-#!/usr/bin/env python
-"""
-Very simple HTTP server in python.
-Usage::
-    ./dummy-web-server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-"""
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+#!/usr/bin/python3
 
-class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
+hostName = ""
+hostPort = 80
+
+class MyServer(BaseHTTPRequestHandler):
+
+    #   GET is for clients geting the predi
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        self.send_response(200)
+        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
 
-    def do_HEAD(self):
-        self._set_headers()
-        
+    #   POST is for submitting data.
     def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    httpd.serve_forever()
 
-if __name__ == "__main__":
-    from sys import argv
+        print( "incomming http: ", self.path )
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        self.send_response(200)
+
+        client.close()
+
+        #import pdb; pdb.set_trace()
+
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))```
